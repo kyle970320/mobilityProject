df1d72871c8479bf5d75bbc9709350116c942f5b
LAPTOP-1FO0QCOS\park9 <park970320@gmail.com>
2022-11-02 20:36:04 +0900

    Add: 필요한 파일들 추가
    App 컴포넌트 삭제 후, 라우팅을 위한 컴포넌트와 api 연동을 위한 파일 하나 추가

--- src/Routing.jsx
+++ src/Routing.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
-const App = () => {
+const Routing = () => {
   return (
     <div>
-      asdasdfads
+      
     </div>
   );
 };
 
-export default App;
+export default Routing;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import Routing from './Routing';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   /* React.StrictMode */
   <>
-    <App />
+    <Routing />
   </>
 );
