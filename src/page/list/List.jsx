63dfdc73d76e4bad2669861b5adf0b1d1d402e49
LAPTOP-1FO0QCOS\park9 <park970320@gmail.com>
2022-11-03 04:23:59 +0900

    Style: list page 에 css로 디자인 추가

--- src/page/list/List.jsx
+++ src/page/list/List.jsx
@@ -3,16 +3,38 @@ import { useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import Loading from '../../comp/Loading';
 import AxiosApi from '../../utils/AxiosApi';
+import style from '../../style/list.module.css'
 
 const List = () => {
   const [stateLoading, setLoading] = useState()
   const [stateList, setList] = useState()
 
+  const changeSeg = (seg)=>{
+    if(seg =='C'){
+      return '소형';
+    } else if(seg == 'D'){
+      return '중형';
+    } else if(seg == 'E'){
+      return '대형';
+    } else{
+      return 'SUV';
+    } 
+  }
+
+  const changeFuelType = (type)=>{
+    if(type =='gasoline'){
+      return '가솔린';
+    } else if(type == 'ev'){
+      return '전기';
+    } else{
+      return '하이브리드';
+    } 
+  }
+
   const getList = async (fuelType, segment) => {
     setLoading(true)
     const listData = await AxiosApi(fuelType, segment);
     setList(listData.data.payload)
-    console.log(listData.data.payload)
     setLoading(false)
     return listData.data;
   }
@@ -22,12 +44,13 @@ const List = () => {
   }, [])
 
   return (
-    <React.Fragment>
-      <div>
+    <div>
+      <div className={style.btnBox}>
         <button onClick={() => getList()}>전체</button>
         <button onClick={() => getList('', "C")}>소형</button>
         <button onClick={() => getList('', "D")}>중형</button>
-        <button onClick={() => getList('', "SUV")}>대형</button>
+        <button onClick={() => getList('', "E")}>대형</button>
+        <button onClick={() => getList('', "SUV")}>SUV</button>
       </div>
       {stateLoading && <Loading />}
       {
@@ -35,15 +58,21 @@ const List = () => {
         &&
         stateList.map((listData) => {
           return (
-            <Link to='/detail' state={listData} key={listData.id}>
-              <ul key={listData.id}>
+            <Link to='/detail' state={listData} key={listData.id} className={style.link}>
+              <ul>
                 <li>{listData.attribute.brand}</li>
+                <li>{listData.attribute.name}</li>
+                <li><span>{changeSeg(listData.attribute.segment)}</span>/<span>{changeFuelType(listData.attribute.fuelType)}</span></li>
+                <li>월 {listData.amount/10000}만원부터</li>
               </ul>
+              <figure>
+                <img src={listData.attribute.imageUrl} alt="" />
+              </figure>
             </Link>
           )
         })
       }
-    </React.Fragment>
+    </div>
   );
 };
 
